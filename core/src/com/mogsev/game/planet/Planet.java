5ea21ffb2f6fbc000663a67f3190b83b3b89f4c4
zhenya <mogsev@gmail.com>
2015-08-29 13:24:19 +0300

    update code

--- core/src/com/mogsev/game/planet/Planet.java
+++ core/src/com/mogsev/game/planet/Planet.java
@@ -3,12 +3,11 @@ package com.mogsev.game.planet;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.mogsev.game.entity.Enemy;
-import com.mogsev.game.util.LoadTexture;
 
 /**
  * Created by zhenya on 28.08.2015.
@@ -16,13 +15,31 @@ import com.mogsev.game.util.LoadTexture;
 public class Planet extends Actor {
     private static final String TAG = "Planet";
     private final Texture texture = new Texture(Gdx.files.internal("planet.png"));
-    //private final TextureRegion region = new TextureRegion(texture, 500, 500);
     private float deltaTime;
+    private float limitX;
+    private float limitY;
+    private float minDuration;
+    private float maxDuration;
+    private float limitTime;
+    private boolean isEnemy;
 
     public Planet() {
         setSize(400, 400);
-        setPosition(MathUtils.random(50), MathUtils.random(50));
-        addAction(Actions.moveTo(300, 300, 150));
+        setLimitTime(4.0f, true);
+        setLimitXY(Gdx.graphics.getWidth() - 200, 400.0f);
+        setDuration(15.0f, 30.0f);
+        setPosition(getRandomX(), getRandomY());
+        addAction(Actions.moveTo(getRandomX(), getRandomY(), getRandomDuration()));
+    }
+
+    private void setLimitXY(float limitX, float limitY) {
+        this.limitX = limitX;
+        this.limitY = limitY;
+    }
+
+    private void setDuration(float minDuration, float maxDuration) {
+        this.minDuration = minDuration;
+        this.maxDuration = maxDuration;
     }
 
     @Override
@@ -30,28 +47,48 @@ public class Planet extends Actor {
         batch.draw(texture, getX(), getY(), getWidth(), getHeight());
     }
 
-    public Actor hit(float x, float y, boolean touchable) {
-        return x > 0 && x < getWidth() && y > 0 && y < getHeight() ? this : null;
-    }
-
     @Override
     public void act(float delta) {
         super.act(delta);
-        deltaTime += delta;
+        newAction();
+        newEnemy(delta);
+    }
+
+    public Actor hit(float x, float y, boolean touchable) {
+        if (touchable && getTouchable() != Touchable.enabled) return null;
+        return x > 0 && x < getWidth() && y > 0 && y < getHeight() ? this : null;
+    }
+
+    private void newAction() {
         if (!hasActions()) {
-            int x = MathUtils.random(350);
-            int y = MathUtils.random(300);
-            if (x < 100) {
-                x -= 400;
-            }
-            if (y < 100) {
-                y -= 400;
-            }
-            addAction(Actions.moveTo(x, y, MathUtils.random(100, 150)));
+            addAction(Actions.moveTo(getRandomX(), getRandomY(), getRandomDuration()));
         }
-        if (deltaTime > 4.0f) {
-            this.getParent().addActor(new Enemy());
-            deltaTime = 0;
+    }
+
+    private void newEnemy(float delta) {
+        if (isEnemy) {
+            deltaTime += delta;
+            if (deltaTime > limitTime) {
+                this.getParent().addActor(new Enemy());
+                deltaTime = 0;
+            }
         }
+    }
+
+    private float getRandomX() {
+        return MathUtils.random(limitX);
+    }
+
+    private float getRandomY() {
+        return MathUtils.random(limitY) - getHeight();
+    }
+
+    private float getRandomDuration() {
+        return MathUtils.random(minDuration, maxDuration);
+    }
+
+    public void setLimitTime(float limitTime, boolean isEnemy) {
+        this.limitTime = limitTime;
+        this.isEnemy = isEnemy;
     }
 }
